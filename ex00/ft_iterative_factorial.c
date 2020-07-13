55b862e517edbdddfcddb5c9513d99b53c3cf22e
joovertoJHB <jooverto@student.wethinkcode.co.za>
2020-07-13 13:59:05 +0200

    Fixed ex00

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -6,20 +6,19 @@
 /*   By: jooverto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 11:38:44 by jooverto          #+#    #+#             */
-/*   Updated: 2020/07/13 12:36:29 by jooverto         ###   ########.fr       */
+/*   Updated: 2020/07/13 13:58:35 by jooverto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include 	<unistd.h>
-#include 	<stdio.h>
 
 int		ft_iterative_factorial(int nb)
 {
 	if ( nb > 1)
 	{
-		return(nb * ft_iterative_factorial(nb - 1));
+		return(nb * (nb - 1));
 	}
-	return nb;	
+	return(0);	
 }
 
 
