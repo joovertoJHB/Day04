9f0cfa5280a6d1016eb182a9a8199d040ed989b3
joovertoJHB <jooverto@student.wethinkcode.co.za>
2020-07-16 11:43:21 +0200

    Fixed errors

--- ex05/ft_sqrt.c
+++ ex05/ft_sqrt.c
@@ -1,27 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jooverto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/13 13:02:17 by jooverto          #+#    #+#             */
-/*   Updated: 2020/07/16 10:59:52 by jooverto         ###   ########.fr       */
+/*   Created: 2020/07/16 10:19:18 by jooverto          #+#    #+#             */
+/*   Updated: 2020/07/16 10:24:37 by jooverto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int		ft_recursive_factorial(int nb)
+int		ft_sqrt(int nb)
 {
-	if (nb > 1)
-	{
-		return(nb * ft_recursive_factorial(nb - 1));
-	}
-	else
-	{
-		return(1);
-		
+	
 }
-
-
